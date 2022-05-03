6464de18c35036c7ddc732fe61a3855d74891aee
Dmitriy Isaev <morok2devil@gmail.com>
2022-05-03 21:09:47 +0400

    macpro commit

---
+++ app/src/main/java/ru/kitfactory/mixingautopaint/presentation/editFragment/EditFragment.kt
@@ -0,0 +1,32 @@
+package ru.kitfactory.mixingautopaint.presentation.editFragment
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.kitfactory.mixingautopaint.R
+
+class EditFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = EditFragment()
+    }
+
+    private lateinit var viewModel: EditViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.edit_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(EditViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/ru/kitfactory/mixingautopaint/presentation/editFragment/EditViewModel.kt
@@ -0,0 +1,7 @@
+package ru.kitfactory.mixingautopaint.presentation.editFragment
+
+import androidx.lifecycle.ViewModel
+
+class EditViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
