66dee9f4e87bc912d06e183979e3c1e3b318c044
Dmitriy Isaev <morok2devil@gmail.com>
2022-05-22 01:50:55 +0400

    move data to package (mac pro commit)

--- app/src/main/java/ru/kitfactory/mixingautopaint/presentation/addFragment/AddMixPaintViewModel.kt
+++ app/src/main/java/ru/kitfactory/mixingautopaint/presentation/addFragment/AddMixPaintViewModel.kt
@@ -2,7 +2,6 @@ package ru.kitfactory.mixingautopaint.presentation.addFragment
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
