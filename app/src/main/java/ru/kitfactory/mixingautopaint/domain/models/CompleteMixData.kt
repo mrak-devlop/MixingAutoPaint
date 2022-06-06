153116c0f9f9028a497a3e3572752615f5c126cb
mrak-devlop <morok2devil@gmail.com>
2022-06-07 02:40:17 +0400

    update (mac pro commit)

---
+++ app/src/main/java/ru/kitfactory/mixingautopaint/domain/models/CompleteMixData.kt
@@ -0,0 +1,9 @@
+package ru.kitfactory.mixingautopaint.domain.models
+
+data class CompleteMixData(
+    val massPaint: Float,
+    val massHardener: Float,
+    val paintPlusHardener: Float,
+    val massDiluent: Float,
+    val massMix: Float
+)
