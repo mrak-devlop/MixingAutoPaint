e1313615c71862979648ddfd75f66b84eacf8756
mrak-devlop <morok2devil@gmail.com>
2022-06-07 01:28:27 +0400

    stable (mac pro commit)

--- app/src/main/java/ru/kitfactory/mixingautopaint/di/AppComponent.kt
+++
@@ -1,9 +0,0 @@
-package ru.kitfactory.mixingautopaint.di
-
-import dagger.Component
-import ru.kitfactory.mixingautopaint.MainActivity
-
-@Component(modules = [DataModule::class])
-interface AppComponent {
-    fun inject(mainActivity: MainActivity)
-}
--- app/src/main/java/ru/kitfactory/mixingautopaint/di/DomainModule.kt
+++
@@ -1,7 +0,0 @@
-package ru.kitfactory.mixingautopaint.di
-
-import dagger.Module
-
-@Module
-class DomainModule {
-}
