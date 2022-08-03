0cb80e70bdc284029c7ec3e3fb435df8f9804f81
KitFactory <74393298+mrak-devlop@users.noreply.github.com>
2022-08-04 01:28:04 +0400

    Delete domain directory

--- domain/src/main/java/ru/kitfactory/mixingautopaint/domain/models/CompleteMixData.kt
+++
@@ -1,9 +0,0 @@
-package ru.kitfactory.mixingautopaint.domain.models
-
-data class CompleteMixData(
-    val massPaint: Float,
-    val massHardener: Float,
-    val paintPlusHardener: Float,
-    val massDiluent: Float,
-    val massMix: Float
-)
